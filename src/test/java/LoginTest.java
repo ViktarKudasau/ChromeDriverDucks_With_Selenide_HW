5e7204910b42029c9cd243e49b0cb7e7208d5266
Viktar Kudasau <viktar.kudasau@gmail.com>
2023-04-13 13:43:36 +0300

    Add Selenide to the MainPage and to theRubberDucksPage

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -6,7 +6,7 @@ public class LoginTest extends BaseTest {
     String Expected_Success_Message = "You are now logged in as Viktar Kudasau.";
     String Expected_Error_Message = "Wrong password or the account is disabled, or does not exist";
 
-    @Test(invocationCount = 1, priority = 0)
+    @Test
     public void TestIncorrectLogin() {
         mainPage.fillIncorrectLoginAndSend();
         Assert.assertEquals(mainPage.getErrorMessage(), Expected_Error_Message,
